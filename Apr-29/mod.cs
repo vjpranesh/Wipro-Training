2675fd7900205d2367474ffbe15d299ef1586e4b
Pranesh <star@microsoft.com>
2025-04-30 11:04:00 +0530

    3

---
+++ Apr-29/mod.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class HelloWorld
+{
+    static int mod(int a,int b){
+        return a%b;
+    
+    }
+    
+     static void Main()
+    
+    {
+        int num1=20;
+        int num2=10;
+        
+        Console.WriteLine("Multiplication: "+mod(num1,num2));
+        
+    }
+}
