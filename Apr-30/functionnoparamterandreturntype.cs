c5fd9076d8897e7d0b72b64bed5c59a44a08c4ab
Pranesh <star@microsoft.com>
2025-04-30 12:58:11 +0530

    function3

---
+++ Apr-30/functionnoparamterandreturntype.cs
@@ -0,0 +1,13 @@
+using System;
+
+class program
+{
+    static void PrintMessage()
+    {
+        Console.WriteLine("Welcome to ChatGPT AI");
+    }
+    static void Main()
+    {
+        PrintMessage();
+    }
+}
