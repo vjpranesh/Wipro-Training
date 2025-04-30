b7db80ef0cb96f09901ce893bea68523136ff6fc
Pranesh <star@microsoft.com>
2025-04-30 12:03:21 +0530

    student

---
+++ Apr-29/student.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int age=25;
+        string name = "John";
+        bool isStudent = true;
+        double fees = 55000.75;
+        
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("is Student: " + isStudent);
+        Console.WriteLine("Fees: " + fees);
+    }
+}
