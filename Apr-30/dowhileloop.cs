3c4774397598ae87962fd326546f8f70ce8146b3
Pranesh <star@microsoft.com>
2025-04-30 10:54:17 +0530

    2

---
+++ Apr-30/dowhileloop.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int i =6;
+        do
+        {
+            Console.WriteLine(i);
+            i++;
+        }while(i<=5);
+    }
+}
