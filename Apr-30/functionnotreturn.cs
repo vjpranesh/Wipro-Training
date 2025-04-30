67106ef0e34baafe004bc6915e78dabce91c447e
Pranesh <star@microsoft.com>
2025-04-30 12:52:54 +0530

    function

---
+++ Apr-30/functionnotreturn.cs
@@ -0,0 +1,13 @@
+using System;
+
+class program
+{
+    static void GreetUser(string name)
+    {
+        Console.WriteLine("Hello, " + name);
+    }
+    static void Main()
+    {
+        GreetUser("Pranesh");
+    }
+}
