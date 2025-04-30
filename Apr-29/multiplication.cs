5f136c3b932d64208cf7dab5fbf6a1af2e8349c8
Pranesh <star@microsoft.com>
2025-04-30 11:05:43 +0530

    4

---
+++ Apr-29/multiplication.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        
+        for(int i=1;i<=10;i++){
+            int n=5;
+            Console.WriteLine ("{0} X {1} ={2} \n",n,i,n*i);
+        
+        }
+    }
+}
