b25bb269cd9a8f5aced18b26cdbbc21d0aa7bdf7
Pranesh <star@microsoft.com>
2025-04-30 10:53:29 +0530

    1

---
+++ Apr-30/forloop.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+     for (int i = 1;i<=5;i++)
+     {
+         Console.WriteLine(i);
+     }
+        }
+}
