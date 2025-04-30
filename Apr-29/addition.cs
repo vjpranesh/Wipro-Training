05967d4bfacabbaef497feb69f074fa847f0e9a9
Pranesh <star@microsoft.com>
2025-04-30 11:01:56 +0530

    1

---
+++ Apr-29/addition.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class HelloWorld
+{
+    static int add(int a,int b){
+        return a+b;
+    
+    }
+    
+     static void Main()
+    
+    {
+        int num1=20;
+        int num2=10;
+        
+        Console.WriteLine("Addition: "+add(num1,num2));
+        
+    }
+}
