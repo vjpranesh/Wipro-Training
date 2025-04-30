825068bfcdd7f4ea8b4ea0786c0899424f1857dd
Pranesh <star@microsoft.com>
2025-04-30 11:03:11 +0530

    2

---
+++ Apr-29/divide.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class HelloWorld
+{
+    static int div(int a,int b){
+        return a/b;
+    
+    }
+    
+     static void Main()
+    
+    {
+        int num1=20;
+        int num2=10;
+        
+        Console.WriteLine("Divide: "+div(num1,num2));
+        
+    }
+}
