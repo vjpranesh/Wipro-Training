7e84a0a87d6d2cbade169a0369c747d06da5468e
Pranesh <Star@microsoft.com>
2025-05-05 10:25:49 +0530

    sql

--- May-05/ex1TotalRevenue.sql
+++ May-05/ex1TotalRevenue.sql
@@ -1,1 +1,15 @@
 CREATE 
+CREATE PROCEDURE GetProductRevenue
+       @ProductID INT
+AS
+BEGIN 
+     SELECT
+         @ProductID AS ProductID,
+         SUM(o.Quantity * p.Price) AS TotalRevenue
+	 FROM Orders o
+	 INNER JOIN Products p ON o.ProductID = p.ProductID
+	 WHERE o.ProductID = @ProductID
+END;
+
+EXEC 
+EXEC GetProductRevenue @ProductID = 1;
