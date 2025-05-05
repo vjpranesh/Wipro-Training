d357e7da7d8f15b2fd326493521995db6acaa7a8
Pranesh <Star@microsoft.com>
2025-05-05 11:48:41 +0530

    sql

---
+++ May-05/Student.sql
@@ -0,0 +1,16 @@
+CREATE
+CREATE TABLE Students (
+     StudentID INT PRIMARY KEY,
+	 FirstName VARCHAR(50),
+	 Marks INT,
+	 DOB DATE
+);
+
+INSERT
+INSERT INTO Students (StudentID, FirstName, Marks, DOB)
+VALUES
+(1, 'Amit', 92, '2003-05-10'),
+(2, 'Neha', 78, '2002-11-15'),
+(3, 'Rahul', 61, '2004-01-20'),
+(4, 'Sneha', 49, '2003-09-01');
+
