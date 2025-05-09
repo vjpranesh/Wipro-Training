fd06ae7a4ed11d86fd5ff23c7e7d9eab45f6d20c
Pranesh <Star@microsoft.com>
2025-05-09 11:49:35 +0530

    class

--- May-09/Assignments/employee.cs
+++ May-09/Assignments/employee.cs
@@ -1,50 +1,33 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Employee
+namespace ConsoleApp3
 {
-    public int EmployeeID;
-    public string Name;
-    public string Department;
-    public double Salary;
+    class Employee
+    {
+        public int EmployeeID;
+        public string Name;
+        public string Department;
+        public double Salary;
 
-    public void PrintDetails()
-    {
-        Console.WriteLine("Employee ID: " + EmployeeID);
-        Console.WriteLine("Name: " + Name);
-        Console.WriteLine("Department:" + Department);
-        Console.WriteLine("Salary:" + Salary);
-    }
-    public void UpdateSalary(double newSalary)
-    {
-        Salary = newSalary;
-    }
-    public double AnnualSalary()
-    {
-        return Salary * 12;
+        public void PrintDetails()
+        {
+            Console.WriteLine("Employee ID: " + EmployeeID);
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Department:" + Department);
+            Console.WriteLine("Salary:" + Salary);
+        }
+        public void UpdateSalary(double newSalary)
+        {
+            Salary = newSalary;
+        }
+        public double AnnualSalary()
+        {
+            return Salary * 12;
+        }
+
     }
 }
-
-class Program
-{
-    static void Main()
-    {
-        Employee emp1 = new Employee { EmployeeID = 1, Name = "Ram", Department = "IT", Salary = 70000 };
-        //emp1.EmployeeID = 1;
-        //emp1.Name = "Ram";
-        //emp1.Department = "IT";
-        //emp1.Salary = 70000;
-
-        emp1.PrintDetails();
-        Console.WriteLine("\nUpdate Salary");
-        emp1.UpdateSalary(80000);
-
-        emp1.PrintDetails();
-
-        Console.WriteLine("\nAnnual Salary: $" + emp1.AnnualSalary());
-    }
-}
-
-
-
-
-
