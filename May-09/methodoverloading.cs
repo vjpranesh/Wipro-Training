b409cdb715e12f4dc567b7f6bd8d2ae295e934d2
Pranesh <Star@microsoft.com>
2025-05-09 14:54:48 +0530

    overloading

---
+++ May-09/methodoverloading.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Calculator
+    {
+        // Addition overloading // Method overloading 
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+}
+
+
