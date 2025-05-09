d485dfa1f5c8585d0d9a1a20894b4e774c381ba1
Pranesh <Star@microsoft.com>
2025-05-09 11:50:40 +0530

    class

---
+++ May-09/Assignments/employeeclass.cs
@@ -0,0 +1,46 @@
+using System;
+
+class Employee
+{
+    public int EmployeeID;
+    public string Name;
+    public string Department;
+    public double Salary;
+
+    public void PrintDetails()
+    {
+        Console.WriteLine("Employee ID: " + EmployeeID);
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Department:" + Department);
+        Console.WriteLine("Salary:" + Salary);
+    }
+    public void UpdateSalary(double newSalary)
+    {
+        Salary = newSalary;
+    }
+    public double AnnualSalary()
+    {
+        return Salary * 12;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Employee emp1 = new Employee { EmployeeID = 1, Name = "Ram", Department = "IT", Salary = 70000 };
+        //emp1.EmployeeID = 1;
+        //emp1.Name = "Ram";
+        //emp1.Department = "IT";
+        //emp1.Salary = 70000;
+
+        emp1.PrintDetails();
+        Console.WriteLine("\nUpdate Salary");
+        emp1.UpdateSalary(80000);
+
+        emp1.PrintDetails();
+
+        Console.WriteLine("\nAnnual Salary: $" + emp1.AnnualSalary());
+    }
+}
+
