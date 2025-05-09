6eaa9ec272e1fadd6710941b035abffde8b380da
Pranesh <Star@microsoft.com>
2025-05-09 12:52:30 +0530

    task

--- May-09/Assignments/task1.cs
+++ May-09/Assignments/task1.cs
@@ -44,3 +44,41 @@ class Program
     }
 }
 
+
+
+
+//adding class 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Employee
+    {
+        public int EmployeeID;
+        public string Name;
+        public string Department;
+        public double Salary;
+
+        public void PrintDetails()
+        {
+            Console.WriteLine("Employee ID: " + EmployeeID);
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Department:" + Department);
+            Console.WriteLine("Salary:" + Salary);
+        }
+        public void UpdateSalary(double newSalary)
+        {
+            Salary = newSalary;
+        }
+        public double AnnualSalary()
+        {
+            return Salary * 12;
+        }
+
+    }
+}
+
