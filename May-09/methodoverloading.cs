5795259b53a311f870cf7614ba77c88a6acfb6b9
Pranesh <Star@microsoft.com>
2025-05-09 15:12:20 +0530

    overloading

--- May-09/methodoverloading.cs
+++ May-09/methodoverloading.cs
@@ -1,3 +1,5 @@
+//calculator.cs
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,7 +23,67 @@ namespace ConsoleApp3
         {
             return a + b + c;
         }
-    }
+
+        //subtraction overlaoding
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public double Sub(double a, double b)
+        {
+            return a - b;
+        }
+        public int Sub(int a, int b, int c)
+        {
+            return (a - b) + c;
+        }
+
+        //multiplication overloading 
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+        public double Mul(double a, double b)
+        {
+            return a * b;
+        }
+        public int Mul(int a, int b, int c)
+        {
+            return a * b + c;
+        }
+
+
+    }   
 }
 
 
+
+//program.cs
+
+using ConsoleApp3;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Calculator calc = new Calculator();
+
+        Console.WriteLine("Addition:");
+        Console.WriteLine("2 + 3 = " + calc.Add(2, 3));
+        Console.WriteLine("2.5 + 3.7 =  " + calc.Add(2.5, 3.7));
+        Console.WriteLine("1 + 2 + 3 = " + calc.Add(1, 2, 3));
+
+        Console.WriteLine("Subtraction:");
+        Console.WriteLine("7 - 3 = " + calc.Sub(7, 3));
+        Console.WriteLine("7.5 - 2.7 =  " + calc.Sub(7.5, 2.7));
+        Console.WriteLine("1 - 2 + 3 = " + calc.Sub(1, 2, 3));
+
+        Console.WriteLine("Multiplication:");
+        Console.WriteLine("2 * 3 = " + calc.Mul(2, 3));
+        Console.WriteLine("2.5 * 3.7 =  " + calc.Mul(2.5, 3.7));
+        Console.WriteLine("1 * 2 + 3 = " + calc.Mul(1, 2, 3));
+
+
+    }
+}
