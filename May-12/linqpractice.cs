156c27738d075063923e6d89e11b1cf00f387321
Pranesh <Star@microsoft.com>
2025-05-12 16:05:03 +0530

    linq

--- May-12/linqpractice.cs
+++ May-12/linqpractice.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 
-namespace Linq_Practice
+namespace Linq_Practice //language integrated query
 {
     class Program
     {
