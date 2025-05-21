3d4b9d20630f9bca6d1b2d953aaa0626c4bf1960
Pranesh <Star@microsoft.com>
2025-05-21 09:38:15 +0530

    task

---
+++ May-21/towerofhanoi.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Program
+{
+    static void TowerOfHanoi(int n, char from, char to, char aux)
+    {
+        if ( n == 1)
+        {
+            Console.WriteLine($"Move disk 1 from {from} to {to}");
+            return;
+        }
+        TowerOfHanoi(n - 1, from, aux, to);
+
+        Console.WriteLine($"Move disk {n} form {from} to {to}");
+
+        TowerOfHanoi(n - 1, aux, to, from);
+    }
+    static void Main()
+    {
+        Console.WriteLine("Enter the number of disks:");
+        int n = int.Parse(Console.ReadLine());
+
+        TowerOfHanoi(n, 'A', 'B', 'C');
+    }
+}
