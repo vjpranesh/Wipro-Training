ca965194f330fc34720ebc3e684c191b80473c49
Pranesh <Star@microsoft.com>
2025-05-30 17:08:07 +0530

    Web API

---
+++ May-30/Assignments/WebApplication15/WebApplication15/Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication15.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public string Post()
+        {
+            return "This is a Post";
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut]
+        public string Put()
+        {
+            return "This is a Put";
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete]
+        public string Delete() 
+        {
+            return "This is a Delete";
+        }
+    }
+}
