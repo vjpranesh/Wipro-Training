715ad14b807c1f56e635d032ce995fae1b7f5c05
Pranesh <Star@microsoft.com>
2025-05-31 11:01:35 +0530

    Quiz application

--- May-31/Exercise3.cs
+++ May-31/Exercise3.cs
@@ -5,12 +5,14 @@ class Question
     public string Text { get; set; }
     public List<string> Options { get; set; }
     public int CorrectOptionIndex { get; set; }
+
     public Question(string text, List<string> options, int correctOptionIndex)
     {
         Text = text;
         Options = options;
         CorrectOptionIndex = correctOptionIndex;
     }
+    
     public bool Ask()
     {
         Console.WriteLine(Text);
