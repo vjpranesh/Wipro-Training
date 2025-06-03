75e4b35ce14f0644e2b7fb2dff4e74b14bbddc28
Pranesh <Star@microsoft.com>
2025-06-03 10:14:43 +0530

    Product API Controller with Http verbs

---
+++ Jun-03/Assignments/WebApplication19/WebApplication19/Data/WebApplication19Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication19.Models;
+
+namespace WebApplication19.Data
+{
+    public class WebApplication19Context : DbContext
+    {
+        public WebApplication19Context (DbContextOptions<WebApplication19Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication19.Models.Product> Product { get; set; } = default!;
+    }
+}
